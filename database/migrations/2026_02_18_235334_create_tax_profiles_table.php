3dbb14f38f242acdca8d1ac74789586c086e565e
jocelin kisenga <kisengajocelin7@gmail.com>
2026-02-19 02:07:29 +0200

    [feat ] added TaxProfile model and filed migrations attributes

--- database/migrations/2026_02_18_235334_create_tax_profiles_table.php
+++ database/migrations/2026_02_18_235334_create_tax_profiles_table.php
@@ -4,6 +4,8 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
+use function Laravel\Prompts\table;
+
 return new class extends Migration
 {
     /**
@@ -13,6 +15,17 @@ return new class extends Migration
     {
         Schema::create('tax_profiles', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->enum('filing_status', ['single','married_joint','married_separeted','head_household'])->default('single');
+            $table->double('annual_income',15,2)->default(0.00);
+            $table->string('country')->nullable();
+            $table->string('state')->nullable();
+            $table->string('town')->nullable();
+            $table->integer('depends')->nullable();
+            $table->double('business_income',15,2)->default(0.00);
+            $table->double('other_income',15,2)->default(0.00);
+            $table->boolean('crypto_activity')->default(false);
+            $table->json('raw_payload')->nullable();
             $table->timestamps();
         });
     }
