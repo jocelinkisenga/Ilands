7a53b75810a95d12939e1f61b0696ebdb0194250
jocelin kisenga <kisengajocelin7@gmail.com>
2026-05-31 18:37:33 +0200

    fix stripe route

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,6 +14,9 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
      //   $middleware->append(AdminMiddleware::class);
+     $middleware->validateCsrfTokens(except: [
+        'stripe/webhook',
+    ]);
         $middleware->alias([
         'admin' => \App\Http\Middleware\AdminMiddleware::class,
         'subscribed'=>
