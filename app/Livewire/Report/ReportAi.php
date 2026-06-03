6140b3f7572eb05a63a254ca65014e0e47a608b2
jocelin kisenga <kisengajocelin7@gmail.com>
2026-06-04 01:07:54 +0200

    updated report file

---
+++ app/Livewire/Report/ReportAi.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Report;
+
+use Livewire\Component;
+
+class ReportAi extends Component
+{
+    public function render()
+    {
+        return view('livewire.report.report-ai');
+    }
+}
