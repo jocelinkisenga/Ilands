7e62a98e59591370b8b1fec8770fad5445f9afdd
jocelin kisenga <kisengajocelin7@gmail.com>
2026-06-27 11:12:04 +0200

    fixed reports page

--- app/Http/Controllers/Report/ReportController.php
+++ app/Http/Controllers/Report/ReportController.php
@@ -7,9 +7,9 @@ use Illuminate\Http\Request;
 use App\Models\AiReport;
 class ReportController extends Controller
 {
-    public function show($reportId)
-{
- $report = AiReport::findOrFail($reportId);
-    return view('pages.reports.report-show', compact('report'));
-}
+  public function show($reportId)
+  {
+    $report = AiReport::findOrFail($reportId);
+    return view("client.reports.report-show", compact("report"));
+  }
 }
