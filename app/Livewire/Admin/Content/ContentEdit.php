4a5672e33cfce1b831b0f9c3851e0b83f40efff4
jocelin kisenga <kisengajocelin7@gmail.com>
2026-07-02 10:56:00 +0200

    fix tokens

--- app/Livewire/Admin/Content/ContentEdit.php
+++ app/Livewire/Admin/Content/ContentEdit.php
@@ -9,53 +9,56 @@ use Illuminate\Support\Str;
 
 class ContentEdit extends Component
 {
-    use WithFileUploads;
+  use WithFileUploads;
 
-    public string $title = '';
-    public string $type = 'blog';
-    public string $excerpt = '';
-    public string $content ;
-    public string $access_level = 'free';
-    public $singleContent;
-    
-    public $thumbnail;
-    public $document;
-    public $video; 
+  public string $title = "";
+  public string $type = "blog";
+  public string $excerpt = "";
+  public string $content = "";
+  public string $access_level = "free";
+  public $singleContent;
 
-    public function mount($contentId){
-        $this->singleContent = Content::findOrFail($contentId);
+  public $thumbnail;
+  public $document;
+  public $video;
+
+  public function mount($contentId)
+  {
+    $this->singleContent = Content::findOrFail($contentId);
+    $this->title = $this->singleContent->title;
+    $this->excerpt = $this->singleContent->excerpt;
+  }
+  public function render()
+  {
+    return view("livewire.admin.content.content-edit");
+  }
+
+  public function save()
+  {
+    $thumbnailPath = $this->thumbnail
+      ? $this->thumbnail->store("thumbnails", "public")
+      : null;
+    $documentPath = null;
+    $videoPath = null;
+
+    if ($this->type === "document" && $this->document) {
+      $documentPath = $this->document->store("documents", "public");
+    } elseif ($this->type === "video" && $this->video) {
+      $videoPath = $this->video->store("videos", "public");
     }
-    public function render()
-    {
-        return view('livewire.admin.content.content-edit');
-    }
+    $this->singleContent->update([
+      "title" => $this->title,
+      "slug" => Str::slug($this->title),
+      "type" => $this->type,
+      "excerpt" => $this->excerpt,
+      "content" => $this->content,
+      "access_level" => $this->access_level,
+      "thumbnail" => $thumbnailPath,
+      "document_path" => $documentPath,
+      "video_url" => $videoPath,
+    ]);
 
-    public function save() {
-        $thumbnailPath = $this->thumbnail ? $this->thumbnail->store('thumbnails', 'public') : null;
-        $documentPath = null;
-        $videoPath = null;
-
-        
-        if ($this->type === 'document' && $this->document) {
-            $documentPath = $this->document->store('documents', 'public');
-        } elseif ($this->type === 'video' && $this->video) {
-            $videoPath = $this->video->store('videos', 'public');
-        }
-        $this->singleContent->update(
-            [
-            'title' => $this->title,
-            'slug' => Str::slug($this->title),
-            'type' => $this->type,
-            'excerpt' => $this->excerpt,
-            'content' => $this->content,
-            'access_level' => $this->access_level,
-            'thumbnail' => $thumbnailPath,
-            'document_path' => $documentPath,
-            'video_url' => $videoPath, 
-            ]
-        );
-
-         session()->flash('success', 'Content created successfully.');
-        return redirect()->to('/admin/content');
-    }
+    session()->flash("success", "Content created successfully.");
+    return redirect()->to("/admin/content");
+  }
 }
